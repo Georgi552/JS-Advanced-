8b2c0370f904bee0952d3049ddcabbf15b08d106
Georgi Mihaylov <81894598+Georgi552@users.noreply.github.com>
2022-05-22 15:40:48 +0300

    Add files via upload

---
+++ 04. Rotate Array Exersise.js
@@ -0,0 +1,19 @@
+function solve(arr, num) {
+    for (let i = 0; i < num; i++ ){
+        let lastNum = arr.pop()
+        arr.unshift(lastNum)
+    }
+    console.log(arr.join(" "))
+}
+
+
+
+solve(['1',
+
+'2',
+
+'3',
+
+'4'],
+
+2)

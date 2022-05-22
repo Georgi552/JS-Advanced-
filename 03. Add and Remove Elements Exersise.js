d2c3636fc32b2fd00238e402f6e47db480f21320
Georgi Mihaylov <81894598+Georgi552@users.noreply.github.com>
2022-05-22 13:54:29 +0300

    Add files via upload

---
+++ 03. Add and Remove Elements Exersise.js
@@ -0,0 +1,22 @@
+function solve(arr) {
+    let num = 1;
+    let result = [];
+    
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] == "add"){
+            result.push(i + 1);
+        } else { 
+            result.pop();
+        }
+    }
+
+    if (result.length == 0) { 
+        console.log("Empty")
+    } else { 
+        console.log(result.join("\n"));
+    }
+}
+
+
+
+solve(['add', 'add', 'remove', 'add', 'add'])

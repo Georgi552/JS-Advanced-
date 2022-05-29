8462da9fded1fb7cf3b6080f4acc2320e3a7767c
Georgi Mihaylov <81894598+Georgi552@users.noreply.github.com>
2022-05-29 17:11:28 +0300

    Add files via upload

---
+++ 01. City Record lab.js
@@ -0,0 +1,16 @@
+function solve(city, population, treasury) {
+    let record = {
+    };
+    record.name = city
+    record.population = population
+    record.treasury = treasury
+
+    return record
+}
+
+
+console.log('Tortuga',
+
+7000,
+
+15000);
